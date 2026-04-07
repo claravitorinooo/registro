32a1d5fb371fe3bf4b662bab32cf6b82981fe84b
Maria Clara Vitorino <mariac08f@gmail.com>
2026-04-06 21:57:20 -0300

    mudança java

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-@Entity
-public class Medida {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private Double peso;
-    private Double altura;
-    private LocalDate dataRegistro;
-
-    // Lógica complexa embutida
-    public Double getImc() {
-        return peso / (altura * altura);
-    }
+// Exemplo de código JavaScript real para o script.js
+function enviarDados() {
+    console.log("Enviando medidas para o servidor Java...");
 }
